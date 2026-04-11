d4eb8ec223642fcd427c6a41c57a603821301ee2
feel <mahmoudelfeelig@gmail.com>
2026-04-11 15:03:26 +0200

    if i have to fix another deployment issue i will scream my lungs out man i hate cloudflare

--- app/api/github/repos/route.ts
+++ app/api/github/repos/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { fetchGithubRepos } from '../../../../lib/githubPortfolioData';
 
+export const runtime = 'edge';
+
 export async function GET() {
   try {
     const repos = await fetchGithubRepos();
